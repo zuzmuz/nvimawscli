4e7fc5b2cf883f9ac93ec58a80d3b35f8d6317df
zahersunique <zaher@bandindustries.com>
2024-04-28 00:24:14 +0300

    figuring out creating split window

--- lua/nvimawscli/menu.lua
+++ lua/nvimawscli/menu.lua
@@ -14,34 +14,34 @@ self.services_header =
 self.services = table.concat(services.names, '\n')
 
 function self.load(bufnr, winnr, config)
-
     self.bufnr = bufnr
     self.winnr = winnr
     print("Current buffer: " .. self.bufnr .. " Current window: " .. self.winnr)
     vim.api.nvim_win_set_buf(self.winnr, self.bufnr)
 
-    vim.api.nvim_buf_set_keymap(self.bufnr, 'n', '<CR>', '', { callback = function()
-        local position = vim.api.nvim_win_get_cursor(self.winnr)
-        local service = utils.get_line(self.bufnr, position[1])
+    vim.api.nvim_buf_set_keymap(self.bufnr, 'n', '<CR>', '', {
+        callback = function()
+            local position = vim.api.nvim_win_get_cursor(self.winnr)
+            local service = utils.get_line(self.bufnr, position[1])
 
-        local new_bufnr = utils.create_buffer()
+            local new_bufnr = utils.create_buffer()
 
-        vim.api.nvim_open_win(new_bufnr, false, {
-            width=config.services.width,
-            height=config.services.height,
-        })
-        -- local new_winnr = utils.create_window(new_bufnr, config.menu)
+            vim.api.nvim_open_win(0, false, {
+                split = 'left',
+                win = 0
+            })
+            -- local new_winnr = utils.create_window(new_bufnr, config.menu)
 
-        vim.api.nvim_win_set_width(self.winnr, config.menu.width)
+            vim.api.nvim_win_set_width(self.winnr, config.menu.width)
 
 
 
-        -- print("Current buffer: " .. new_bufnr .. " Current window: " .. new_winnr)
-        -- services.load(service, new_bufnr, new_winnr, config)
-    end})
+            -- print("Current buffer: " .. new_bufnr .. " Current window: " .. new_winnr)
+            -- services.load(service, new_bufnr, new_winnr, config)
+        end
+    })
 
     utils.write_lines(self.bufnr, self.header .. self.services_header .. self.services)
 end
-
 
 return self
