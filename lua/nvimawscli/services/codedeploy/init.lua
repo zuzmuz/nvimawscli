f5fd4849f5a5dfa4834fe98d4c905fdbfd235d6d
zaher hamadeh <hamadehzaher0@gmail.com>
2024-12-26 16:37:48 +0100

    Feature/code deploy (#29)
    * fixing error printing with new lines
    
    * refactored list view to exclude border style
    
    * finished refactoring listview
    
    * removed codedeploy

---
+++ lua/nvimawscli/services/codedeploy/init.lua
@@ -0,0 +1,34 @@
+local config = require('nvimawscli.config')
+local ListView = require('nvimawscli.ui.views.listview')
+
+---@class CodeDeployView: ListView
+local M = setmetatable({}, { __index = ListView })
+
+M.name = 'codedeploy'
+
+function M:fetch_lines(callback)
+    callback {
+        sections = {{
+            lines = {
+                "applications",
+                "deployment_groups",
+                "deployments",
+            }}
+        }
+    }
+end
+
+function M:did_select_item(item)
+    if item.selectable then
+        local subservice_name = item.text
+        local status, subservice = pcall(require, 'nvimawscli.services.codedeploy.' .. subservice_name)
+        if status then
+            subservice:show(config.menu.split)
+            vim.api.nvim_win_set_width(M.winnr, config.menu.width)
+        else
+            vim.api.nvim_err_writeln('Ec2 subservice not found: ' .. subservice_name)
+        end
+    end
+end
+
+return M
