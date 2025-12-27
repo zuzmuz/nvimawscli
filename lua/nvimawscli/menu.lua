455240234629d6c94a134c4b4b988e0a38b48fff
zuzmuz <hamadehzaher0@gmail.com>
2025-12-27 14:32:12 +0100

    added select popup

--- lua/nvimawscli/menu.lua
+++ lua/nvimawscli/menu.lua
@@ -1,5 +1,6 @@
 local config = require('nvimawscli.config')
 local data = require('nvimawscli.utils.data')
+local Iterable = require('nvimawscli.utils.itertools').Iterable
 local ListView = require('nvimawscli.ui.views.listview')
 local popup = require('nvimawscli.ui.popup')
 
@@ -57,42 +58,91 @@ function M:fetch_lines(callback)
     callback(content)
 end
 
+---Prompt user to enter a new profile name
+---@param self MenuView
+local function prompt_for_new_profile(self)
+    if not data.store.profiles then
+        data.store.profiles = {}
+    end
+    popup.create_floating_input(
+        "Enter Profile Name:",
+        20, 1,
+        data.store.current_profile or 'default',
+        config.table,
+        function(input)
+            if input and input ~= "" then
+                data.store.profiles[input] = true
+                data.store.current_profile = input
+                data.save()
+                self:load_content()
+                vim.notify(
+                    "Profile set to: " .. input,
+                    vim.log.levels.INFO
+                )
+            else
+                vim.notify(
+                    "Profile not changed.",
+                    vim.log.levels.WARN
+                )
+            end
+        end
+    )
+end
+
+---Prompt user to select profile from stored profiles or enter a new one.
+---Whenever a new profile is stored it will be available for selection
+---@param self MenuView
+local function prompt_for_profile_select(self)
+    local actions = Iterable(data.store.profiles):keys().table
+    actions[#actions + 1] = "Enter new profile name"
+    popup.create_floating_select(
+        "Select Profile Name:",
+        nil,
+        actions,
+        config.table,
+        function(selected_action)
+            if selected_action < #actions then
+                -- Choose existing profile
+                local profile = actions[selected_action]
+                data.store.current_profile = profile
+                data.save()
+                self:load_content()
+                vim.notify(
+                    "Profile set to: " .. profile,
+                    vim.log.levels.INFO
+                )
+            else
+                -- Enter new profile
+                prompt_for_new_profile(self)
+            end
+        end
+    )
+end
+
+
 ---@param item Line
 function M:did_select_item(item)
     if item.position[1] == 1 then
-        popup.create_floating_input(
-            "Enter Profile Name:",
-            20, 1,
-            data.store.current_profile or 'default',
-            config.table,
-            function(input)
-                if input and input ~= "" then
-                    data.store.current_profile = input
-                    data.save()
-                    vim.notify(
-                        "Profile set to: " .. input,
-                        vim.log.levels.INFO
-                    )
-                    self:load_content()
-                else
-                    vim.notify(
-                        "Profile not changed.",
-                        vim.log.levels.WARN
-                    )
-                end
-            end)
-    end
-    local service_name = item.text
-    local status, service = pcall(require, 'nvimawscli.services.' .. service_name)
-    ---@cast service View
-    if status then
-        service:show(config.menu.split)
-        vim.api.nvim_win_set_width(self.winnr, config.menu.width)
+        -- Selection profile
+        if data.store.profiles then
+            prompt_for_profile_select(self)
+        else
+            prompt_for_new_profile(self)
+        end
     else
-        vim.notify(
-            "Service not implemented yet: " .. service_name,
-            vim.log.levels.WARN
-        )
+        -- Selecting service
+        local service_name = item.text
+        local status, service = pcall(require, 'nvimawscli.services.' .. service_name)
+        ---@cast service View
+        if status then
+            service:show(config.menu.split)
+            vim.api.nvim_win_set_width(self.winnr, config.menu.width)
+        else
+            vim.notify(
+                "Service not implemented yet: " .. service_name,
+                vim.log.levels.WARN
+            )
+        end
     end
 end
 
--- lua/nvimawscli/utils/data.lua
+++ lua/nvimawscli/utils/data.lua
@@ -4,14 +4,11 @@ local data_path = vim.fn.stdpath('data') .. '/nvimawscli.json'
 
 function M.load()
     local file = io.open(data_path, 'r')
-    vim.notify("Loading nvimawscli data from " .. data_path)
     if file then
         local content = file:read('*all')
         M.store = vim.json.decode(content)
-        vim.notify("Loaded nvimawscli data from " .. data_path)
         file:close()
     else
-        vim.notify("No nvimawscli data file found at " .. data_path)
         M.store = {}
         M.save()
     end
@@ -21,7 +18,6 @@ end
 function M.save()
     local file = io.open(data_path, 'w')
     if file then
-        print(vim.inspect(M.store))
         file:write(vim.json.encode(M.store))
         file:close()
     end
